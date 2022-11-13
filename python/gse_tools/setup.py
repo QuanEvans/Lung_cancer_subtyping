a0b51ae64dbac6dbfb73336b65c59f70ff0c95e3
QuanEvans <18670351591@163.com>
2022-11-13 01:15:19 -0500

    update gse class
    change properties access and adding get method for them;
    adding ploting method including pca

---
+++ python/gse_tools/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='GSEs', # Name of the package
+    version='1.0.0', # version of the package
+    description='GSE tools', #  description of the package
+    author='Quancheng Liu', # author of the package
+    py_modules=[
+        'gse_tools.gse', #  the name of the module
+    ] 
+)
